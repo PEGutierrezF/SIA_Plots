1f4c68f583556e8bdc634c2378e9767a999c0963
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-10-24 12:25:02 -0600

    Plots

--- Biplots.R
+++ Biplots.R
@@ -21,28 +21,34 @@ levels(QPA_Feb17$taxa)
                                    expression(italic("P. pulchrus")), "Libellulidae", 
                                    expression(italic("X. elongata")), expression(italic("A. lanipes")),
                                    expression(italic("M. crenulatum")), expression(italic("A. evermani")))) +
+# Segments  
+  geom_segment(aes(x=-31.91,xend=-32.25,yend=0.56,y=0.56), size=2, color="#d95f0e", arrow = arrow(length = unit(0.1, "cm"), ends = "both", angle = 90)) + # C leaflitter
+  geom_segment(aes(x=-32.08,xend=-32.08,yend=0.878914827,y=0.241085173), size=2, color="#d95f0e", arrow = arrow(length = unit(0.1, "cm"),ends = "both", angle = 90)) + # N leaflitter
   
-  geom_segment(aes(x=-31.91,xend=-32.25,yend=0.56,y=0.56), size=2, color="#d95f0e") + # C leaflitter
-  geom_segment(aes(x=-32.08,xend=-32.08,yend=0.878914827,y=0.241085173), size=2, color="#d95f0e") + # N leaflitter
+  geom_segment(aes(x=-31.48,xend=-31.90,yend=6.06,y=6.06), size=2, color="#2c7fb8", arrow = arrow(length = unit(0.1, "cm"), ends = "both",angle = 90))+ # C biofilm
+  geom_segment(aes(x=-31.69,xend=-31.69,yend=6.389520784,y=5.730479216), size=2, color="#2c7fb8", arrow = arrow(length = unit(0.1, "cm"),ends = "both", angle = 90))+ # N biofilm
   
-  geom_segment(aes(x=-31.48,xend=-31.90,yend=6.06,y=6.06), size=2, color="#2c7fb8")+ # C biofilm
-  geom_segment(aes(x=-31.69,xend=-31.69,yend=6.389520784,y=5.730479216), size=2, color="#2c7fb8")+ # N biofilm
-  
-  geom_segment(aes(x=-30.79,xend=-31.81,yend=11.37138181,y=11.37138181), size=2, color="#31a354") +# C algae
-  geom_segment(aes(x=-31.29893075,xend=-31.29893075,yend=17.153228921,y=5.589534699), size=2, color="#31a354") +# algae
+  geom_segment(aes(x=-30.79,xend=-31.81,yend=11.37138181,y=11.37138181), size=2, color="#31a354", arrow = arrow(length = unit(0.1, "cm"), ends = "both",angle = 90)) +# C algae
+  geom_segment(aes(x=-31.29893075,xend=-31.29893075,yend=17.153228921,y=5.589534699), size=2, color="#31a354", arrow = arrow(length = unit(0.1, "cm"),ends = "both", angle = 90)) +# algae
+
+# Axis Limits 
   xlim(-40,-20) +
   ylim(-5,20) +
-
+    
+# Axis
   theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y 
   theme(axis.title.x = element_text(size = 12, angle = 00)) + # axis x
-  
   theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis y
     
+# Legend    
     theme(legend.title = element_text(color = "black", size = 14),
       legend.text = element_text(color = "black", size = 12)) +
     theme_set(theme_gray() + theme(legend.key=element_blank()))  + # gray background
-  
+    theme(legend.text.align = 0) + # text align left legend
+    theme(legend.position = c(0.9, 0.8)) +# Position in a plot. Their values should be between 0 and 1. 
+    
+# Panel
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black")) +
   theme(panel.border = element_rect(colour = "black", fill=NA, size=0.5))
