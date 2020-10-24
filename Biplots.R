342d852ad85696a4204e1429b0e36374733dc55c
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-10-23 22:50:38 -0600

    Plot

--- Biplots.R
+++ Biplots.R
@@ -7,13 +7,20 @@ QPA_Feb17 <- read.csv("Biplot/QPA_Feb17.csv")
 head(QPA_Feb17)
 
 QPA_Feb17$taxa <- factor(QPA_Feb17$taxa, levels = c("Glossosomatidae", "Baetidae", "Chironomidae", "N. julio",
-                                                    "Phylloicus", "Libellulidae", "X. elongata", "A. lanipes",
-                                                    "Machrobrachium", "A. evermani"))
+                                                    "P. pulchrus", "Libellulidae", "X. elongata", "A. lanipes",
+                                                    "M. crenulatum", "A. evermani"))
 levels(QPA_Feb17$taxa) 
 
-  ggplot(QPA_Feb17, aes(x=C, y=N)) +
-  geom_point(aes(colour=taxa), size=4) +
-  labs(x= expression(delta^{13}*"C (???)"), y = expression(delta^{15}*"N (???)"))+
+  ggplot(QPA_Feb17, aes(x=C, y=N, colour=taxa)) +
+  geom_point( size=5) +
+  labs(x= expression(delta^{13}*"C (???)"), y = expression(delta^{15}*"N (???)")) +
+  scale_colour_manual("Taxa",
+                        values = c("#276419", "#4d9221", "#b35806", "#fdb863", "#2166ac", 
+                                   "#D55E00", "#fdae61", "#f46d43", "#d73027", "#003c30"),
+                        labels = c("Glossosomatidae", "Baetidae", "Chironomidae",expression(italic( "N. julio")),
+                                   expression(italic("P. pulchrus")), "Libellulidae", 
+                                   expression(italic("X. elongata")), expression(italic("A. lanipes")),
+                                   expression(italic("M. crenulatum")), expression(italic("A. evermani")))) +
   
   geom_segment(aes(x=-31.91,xend=-32.25,yend=0.56,y=0.56), size=2, color="#d95f0e") + # C leaflitter
   geom_segment(aes(x=-32.08,xend=-32.08,yend=0.878914827,y=0.241085173), size=2, color="#d95f0e") + # N leaflitter
@@ -26,11 +33,14 @@ levels(QPA_Feb17$taxa)
   xlim(-40,-20) +
   ylim(-5,20) +
 
-  theme(axis.title.y = element_text(size = 12, angle = 90)) + 
-  theme(axis.title.x = element_text(size = 12, angle = 00)) +
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y 
+  theme(axis.title.x = element_text(size = 12, angle = 00)) + # axis x
   
   theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
   theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis y
+    
+    theme(legend.title = element_text(color = "black", size = 14),
+      legend.text = element_text(color = "black", size = 12)) +
   
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black")) +
