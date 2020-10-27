264f6d9bde7071f39c53cf8be9d301b1f611a0e0
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-10-26 18:01:31 -0600

    plot

--- Extra.R
+++ Extra.R
@@ -1,5 +1,21 @@
 
 
+
+L <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Leaflitter")
+L
+quantile(L$density, 0.025)
+quantile(L$density, 0.975)
+
+B <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Biofilm")
+quantile(B$density, 0.025)
+quantile(B$density, 0.975)
+
+A <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Algae")
+quantile(A$density, 0.025)
+quantile(A$density, 0.975)
+
+
+
 gF17 <- ggplot(QPA_G_Feb17, aes(x=density, color = source)) + 
   labs(y="Density", x="Source contribution") +
   geom_density(aes(linetype = source), size=1.2) +
---
+++ example.R
@@ -0,0 +1,54 @@
+
+
+set.seed(50)
+
+QPA_G_Feb17 <- data.frame(density = c(rgamma(400, 2, 10), 
+                                      rgamma(400, 2.25, 9),
+                                      rgamma(400, 5, 7)),
+                          source = rep(c("Algae", "Biofilm", "Leaflitter"), 
+                                       each = 400))
+
+dens <- lapply(split(QPA_G_Feb17, QPA_G_Feb17$source), 
+               function(x) density(x$density, from = 0, to = 1))
+
+df <- do.call(rbind, mapply(function(x, y) {
+  data.frame(x = x$x, y = x$y, source = y)
+}, dens, names(dens), SIMPLIFY = FALSE))
+
+df <- df %>% 
+  group_by(source) %>%
+  mutate(cdf = cumsum(y * mean(diff(x))),
+         lower = cdf < 0.025,
+         upper = cdf > 0.975) 
+
+ggplot(df, aes(x, y, color = source)) + 
+  geom_area(data = df[df$lower,], aes(fill = source), alpha = 0.5,
+            position = "identity") +
+  geom_area(data = df[df$upper,], aes(fill = source), alpha = 0.5,
+            position = "identity") +
+  labs(y = "Density", x = "Source contribution") +
+  geom_line(aes(linetype = source), size = 1.2) +
+  scale_fill_manual(values = c("#31a354", "#2c7fb8", "#d95f0e")) +
+  scale_color_manual(values = c("#31a354", "#2c7fb8", "#d95f0e")) +
+  scale_linetype_manual(values = c("solid", "dotted", "longdash")) +
+  theme_classic() +
+  ylim(0, 5) +
+  xlim(0, 1) +
+  theme(axis.text.y  = element_text(size = 12, vjust = 0.5),
+        axis.text.x  = element_text(size = 12, vjust = 0.5),  
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))
+
+
+L <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Leaflitter")
+L
+quantile(L$density, 0.025)
+quantile(L$density, 0.975)
+
+B <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Biofilm")
+quantile(B$density, 0.025)
+quantile(B$density, 0.975)
+
+A <- QPA_G_Feb17 %>% filter(QPA_G_Feb17$source == "Algae")
+quantile(A$density, 0.025)
+quantile(A$density, 0.975)
