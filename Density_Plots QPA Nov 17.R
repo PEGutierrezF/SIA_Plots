d03866c990b969498b3b02a74bfc8b7e3e76a274
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-06-04 14:55:07 -0600

    data

---
+++ Density_Plots QPA Nov 17.R
@@ -0,0 +1,65 @@
+
+
+
+
+# ---------------------------------------------
+# Density Plots November 2017 Quebrada Prieta A
+# 04 Jun 2021
+# Pablo E. Gutiérrez-Fonseca
+# ---------------------------------------------
+#  
+
+
+library(ggplot2)
+library(dplyr)
+library(ggpubr)
+library(patchwork)
+
+
+QPA_G_Nov17 <- read.csv("DensityPlots/QPANov17/01 QPA_Glossosomatidae_Nov17.csv")
+head(QPA_G_Nov17)
+
+
+dens <- lapply(split(QPA_G_Nov17, QPA_G_Nov17$source), 
+               function(x) density(x$density, from = 0, to = 1))
+
+df <- do.call(rbind, mapply(function(x, y) {
+  data.frame(x = x$x, y = x$y, source = y)
+}, dens, names(dens), SIMPLIFY = FALSE))
+
+df <- df %>% group_by(source) %>%
+  mutate(cdf = cumsum(y * mean(diff(x))),
+         lower = cdf < 0.025,
+         upper = cdf > 0.975) 
+
+g1 <- ggplot(df, aes(x, y, color = source)) + 
+  geom_area(data = df[df$lower,], aes(fill = source), alpha = 0.5,
+            position = "identity") +
+  geom_area(data = df[df$upper,], aes(fill = source), alpha = 0.5,
+            position = "identity") +
+  labs(y = "Density", x = "Source contribution") +
+  geom_line(aes(linetype = source), size = 1.2) +
+  scale_fill_manual(values = c("#31a354", "#2c7fb8", "#d95f0e")) +
+  scale_color_manual(values = c("#31a354", "#2c7fb8", "#d95f0e")) +
+  scale_linetype_manual(values = c("solid", "dotted", "longdash")) +
+  theme_classic() +
+  ylim(0, 8) +
+  xlim(0, 1) +
+  ggtitle('Glossosomatidae') +
+  theme(legend.position = "none")+
+  theme(axis.text.y  = element_text(size = 12, vjust = 0.5),
+        axis.text.x  = element_text(size = 12, vjust = 0.5),  
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14))
+
+L <- QPA_G_Nov17 %>% filter(QPA_G_Nov17$source == "Leaflitter")
+quantile(L$density, 0.025)
+quantile(L$density, 0.975)
+
+B <- QPA_G_Nov17 %>% filter(QPA_G_Nov17$source == "Biofilm")
+quantile(B$density, 0.025)
+quantile(B$density, 0.975)
+
+A <- QPA_G_Nov17 %>% filter(QPA_G_Nov17$source == "Algae")
+quantile(A$density, 0.025)
+quantile(A$density, 0.975)
