0619971b9e0fecb97dc270f50d3841e3af9fc761
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-01-28 15:31:41 -0600

    AFDM

---
+++ Figure AFDM.R
@@ -0,0 +1,24 @@
+
+
+
+
+# ---------------------------------------------
+# AFDM Prieta A
+# 28 Jan 2022
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+
+
+# cleans global environment
+rm(list = ls())
+
+
+
+
+libraries <- c( "ggplot2", "plyr","dplyr", 'patchwork','tidyverse')
+lapply(libraries, require, character.only = TRUE)
+
+data <- read.csv("data/AFDM.csv")
