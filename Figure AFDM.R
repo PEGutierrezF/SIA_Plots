7618fec95fc4effc35d75e90016571b4e0ea6ce6
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-01-28 15:47:04 -0600

    AFDM

--- Figure AFDM.R
+++ Figure AFDM.R
@@ -22,3 +22,13 @@ libraries <- c( "ggplot2", "plyr","dplyr", 'patchwork','tidyverse')
 lapply(libraries, require, character.only = TRUE)
 
 data <- read.csv("data/AFDM.csv")
+head(data)
+
+ggplot(data, aes(x=date, y=value) ) + 
+  geom_jitter(position=position_jitter(width=0.3, height=0.2), 
+              alpha=0.9) +
+  geom_boxplot(alpha = 0.5, show.legend = FALSE) + 
+  facet_grid(.~period) +
+  theme(strip.text.x = element_text(size=9, color="black", face="bold"))
+
+
