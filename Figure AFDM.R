d4907d678c1d2aec8d05149ccf422b7665725c39
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-02-01 20:24:19 -0600

    AFDM plot

--- Figure AFDM.R
+++ Figure AFDM.R
@@ -24,9 +24,21 @@ lapply(libraries, require, character.only = TRUE)
 data <- read.csv("data/AFDM.csv")
 head(data)
 
+# First, rename variables
+data$period <- factor(data$period, levels=c('six' ,
+                                            'two', 'nine' ,
+                                            'eighteen' ))
+
+'six' = "6mo pre-",
+'two' = "2mo post-", 'nine' = "9mo post-",
+'eighteen' = "18mo post-"
+
 ggplot(data, aes(x=period, y=value) )+
   geom_violin()+
   geom_jitter(shape=16, position=position_jitter(0.2))+
+  
+  scale_x_discrete(labels=c('6mo pre-','2mo post-','9mo post-', 
+                            "18mo post-")) +
   facet_grid(.~stream) +
   theme(strip.text.x = element_text(size=9, color="black", face="bold"))
 
