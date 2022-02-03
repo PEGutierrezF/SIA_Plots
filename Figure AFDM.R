9f1ab94775d07ba6f835d7693e513955abb9c714
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-02-03 16:47:10 -0500

    AFDM

--- Figure AFDM.R
+++ Figure AFDM.R
@@ -33,11 +33,21 @@ streams <- as_labeller(c(qpa="Prieta A", qpb="Prieta B"))
 
 ggplot(data, aes(x=period, y=value) )+
   geom_violin()+
-  geom_jitter(shape=16, position=position_jitter(0.2))+
+  geom_jitter(width=.1, alpha=.5) +
   
+  labs(x="Sampling period", y= "AFDM value ("*g~m^-2~d^-1*")") +
   scale_x_discrete(labels=c('6mo pre-','2mo post-',
                             '9mo post-', 
                             "18mo post-")) +
+#Axis 
+  theme_bw() +
+  theme(legend.position="none")  +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) +  #subaxis y
+
   facet_grid(.~stream, labeller= streams) +
   theme(strip.text.x = element_text(size=9, color="black", face="bold"))
 
