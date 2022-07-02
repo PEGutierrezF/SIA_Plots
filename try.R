8ec572e416d987f1e0fef775470b90e4be606840
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-07-01 22:53:58 -0400

    data

--- try.R
+++ try.R
@@ -7,12 +7,12 @@ head(QPA_G_Nov17)
 
 ggplot(QPA_G_Nov17, aes(x = density, color = source, 
                         fill = after_stat(ifelse(quantile == 2, NA, color)))) +
-  geom_density_ridges_gradient(aes(y = 0), quantile_lines = TRUE, size=1, 
-                               quantile_fun = hdi, vline_linetype = 0) +
+  geom_density_ridges_gradient(aes(y = 0), quantile_lines = TRUE, size=1.5, 
+                               quantile_fun = hdi, vline_linetype = 0,
+                               bandwidth = 0.1) +
   labs(y = "Density", x = "Source contribution") +
   
- # scale_fill_discrete(guide = "none", na.value = "transparent") + 
-  
+#  scale_fill_discrete(guide = "none", na.value = "transparent") 
   
   scale_fill_cyclical(name = "", values = c("#31a354", "#2c7fb8", "#d95f0e"),
                     labels = c("Algae", "Biofilm", "Leaf litter"),
@@ -22,17 +22,13 @@ ggplot(QPA_G_Nov17, aes(x = density, color = source,
                       labels = c("Algae", "Biofilm", "Leaf litter"),
                       guide = "none", na.value = "transparent") +
 
-  scale_linetype_cyclical(values = c("solid", "dotted", "longdash"),
-                          labels = c("Algae", "Biofilm", "Leaf litter"),
-                          guide = "none", na.value = "transparent") +
-
   theme_classic() +
   ylim(0, 8) +
   xlim(0, 1) +
   ggtitle('Glossosomatidae') +
   theme(plot.title = element_text(face="bold"))+
   
- # theme(legend.position = "none")+
+ # theme(legend.position=c(.9,.75))+
   theme(axis.text.y  = element_text(size = 12, vjust = 0.5),
         axis.text.x  = element_text(size = 12, vjust = 0.5),  
         axis.title.x = element_text(size = 14),
