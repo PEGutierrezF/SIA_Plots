23dddc155e02749040d7c30d078750af4d3c4ae2
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2023-07-29 22:24:18 -0400

    data

---
+++ Figure extra/Chlorophyll vs Discharge.R
@@ -0,0 +1,90 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cleans global environment
+rm(list = ls())
+
+
+# Read as excel .xlsx file
+data <- read_excel("data/physicochemical_data_2017_2019.xlsx", 
+                   sheet = "environmental_var", na = '')
+
+
+
+# Prieta A: Chlorophyll vs Discharge --------------------------------------
+discharge <- data[1:1762,] # Select Discharge
+discharge <- discharge %>% # Select Prieta A
+  filter(stream=="QPA")
+
+chla_QPA <- slice(data, (1931:1960)) # Select Chlorophyll
+
+merged_df <- merge(chla_QPA, discharge, by = "date") # Merge both dataframe by date
+
+
+merged_df$value.x <- as.numeric(as.character(merged_df$value.x))
+shapiro.test(merged_df$value.x)
+
+merged_df$value.y <- as.numeric(as.character(merged_df$value.y))
+shapiro.test(merged_df$value.y)
+
+merged_df <- na.omit(merged_df)
+# Calculate Spearman's rank correlation
+correlation_value_xy <- cor(merged_df$value.x, merged_df$value.y, method = "spearman")
+
+
+print(paste("Correlation (rho):", correlation_value_xy))
+print(paste("P-value:", correlation_value_xy))
+
+library("ggpubr")
+ggscatter(merged_df, x = "value.y", y = "value.x", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "spearman",
+          xlab = "Discharge", ylab = "Chlorophyll-a")
+
+
+
+# Prieta B:  Chlorophyll vs Discharge -------------------------------------
+
+discharge <- data[1:1762,] # Select Discharge
+discharge <- discharge %>% # Select Prieta A
+  filter(stream=="QPA")
+
+chla_QPB <- slice(data, (1961:1990)) # Select Chlorophyll
+tail(chla_QPB)
+merged_df <- merge(chla_QPB, discharge, by = "date") # Merge both dataframe by date
+
+
+merged_df$value.x <- as.numeric(as.character(merged_df$value.x))
+shapiro.test(merged_df$value.x)
+
+merged_df <- na.omit(merged_df)
+merged_df$value.y <- as.numeric(as.character(merged_df$value.y))
+shapiro.test(merged_df$value.y)
+
+merged_df <- na.omit(merged_df)
+# Calculate Spearman's rank correlation
+correlation_value_xy <- cor(merged_df$value.x, merged_df$value.y, method = "spearman")
+
+
+print(paste("Correlation (rho):", correlation_value_xy))
+print(paste("P-value:", correlation_value_xy))
+
+library("ggpubr")
+ggscatter(merged_df, x = "value.y", y = "value.x", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "spearman",
+          xlab = "Discharge", ylab = "Chlorophyll-a")
+
+
+
