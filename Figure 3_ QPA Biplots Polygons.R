1e43bc946d9c449ec29b74a16e1f46ea1d8d8702
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2023-09-21 22:38:40 -0400

    data

--- Figure 3_ QPA Biplots Polygons.R
+++ Figure 3_ QPA Biplots Polygons.R
@@ -523,5 +523,5 @@ p2 <- ggdraw(p1) +
 p3 <- p2 + theme(plot.margin = unit(c(0.5, 2, 0, 0), units = "cm")) # t=1, l=2, b=1, r=1
 p3
 
-p3 + ggsave("Figure 3.jpg", width=11, height=6.5)
+p3 + ggsave("Figure 3.tiff", width=11, height=6.5, dpi=600)
 #
